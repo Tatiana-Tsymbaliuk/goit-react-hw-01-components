79b395cdc2abe27d82ddfc17f81228d1e83d74b6
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-04-29 21:47:32 +0300

    create task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,25 @@
 
 import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+//import {StatisticsBoard} from 'components/StatisticsBoard/StatisticsBoard'
 import user from '../user.json';
+import data from '../data.json';
 //import { Profile } from './Profile/Profile';
 
 
 export const App = () => {
   return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
-    >
+    <div >
       <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
-      stats={user.stats}/>
+      stats={user.stats} />
       
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics  stats={data}/>
+     
     </div>
   );
-};
+}; 
